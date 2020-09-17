e68f1d659db0349054a5c9e9b88f7f72b6d2e938
ke1echi <mrkelechichinaka@gmail.com>
2020-09-17 18:01:27 +0100

    First deployment to heroku

--- server/index.js
+++ server/index.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
+const cors = require('cors');
 
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./Users');
 
 const PORT = process.env.PORT || 8080;
 const router = require('./router');
 
-
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+app.use(router);
+app.use(cors);
+
 io.on('connect', socket => {
-  console.log('connection made!!!');
+  console.log('connection made!!');
 
   socket.on('join', ({ name, room }, callback) => {
     const { error, user } = addUser({ id: socket.id, name, room });
@@ -26,24 +29,26 @@ io.on('connect', socket => {
 
     socket.join(user.room);
 
+    io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+
     callback();
   });
 
 
   socket.on('sendMessage', (message, callback) => {
     const user = getUser(socket.id);
-
     io.to(user.room).emit('message', { user: user.name, text: message });
-
+    io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
     callback();
   });
 
   socket.on('disconnect', () => {
-    console.log('user left');
+    const user = removeUser(socket.id);
+    if (user) {
+      io.to(user.room).emit('message', { user: 'admin', text: `${user.name} left room.`});
+    }
+    console.log('user left!');
   });
 });
-
-app.use(router);
-
 
 server.listen(PORT, () =>console.log(`Server running on port: ${PORT}`));
